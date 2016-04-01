e179dae331ef557129b0e3223af375b23323fba8
Andrew Drobyazko <c2q9450@gmail.com>
2016-04-01 19:50:45 +0300

    Started to impl bottom layout for details activity

--- app/src/main/java/com/nethergrim/unsplashed/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/nethergrim/unsplashed/ui/details/DetailsActivity.kt
@@ -9,12 +9,12 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import com.hannesdorfmann.mosby.mvp.MvpActivity
+import com.nethergrim.unsplashed.R
+import com.nethergrim.unsplashed.utils.dp2px
 import com.nethergrim.unsplashed.utils.hide
 import com.nethergrim.unsplashed.utils.show
 import com.tbruyelle.rxpermissions.RxPermissions
@@ -79,6 +79,26 @@ class DetailsActivity : MvpActivity<DetailsView, DetailsPresenter>(), DetailsVie
         imageView = SubsamplingScaleImageView(this)
         imageView.layoutParams = ViewGroup.LayoutParams(-1, -1)
         rootView.addView(imageView, 1)
+
+
+        val bottomLayout = LinearLayout(this)
+        bottomLayout.setBackgroundColor(resources.getColor(R.color.colorAccent))
+        bottomLayout.orientation = LinearLayout.HORIZONTAL
+        bottomLayout.weightSum = 5f
+
+        val bottomLayoutParams = FrameLayout.LayoutParams(-1, dp2px(48).toInt())
+        bottomLayoutParams.gravity = Gravity.BOTTOM
+        rootView.addView(bottomLayout, bottomLayoutParams)
+
+        val btnShare = ImageButton(this)
+        val shareParams = LinearLayout.LayoutParams(0, -1)
+        shareParams.weight = 1f
+
+
+    }
+
+    fun fabClicked() {
+
     }
 
     override fun showLoadingView() {
