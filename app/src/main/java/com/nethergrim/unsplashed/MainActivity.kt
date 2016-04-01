4d4fdc420a75d83e0231bbf2402305e173740a6f
Andrew Drobyazko <c2q9450@gmail.com>
2016-04-01 12:33:00 +0300

    Changed color schemam style updates

--- app/src/main/java/com/nethergrim/unsplashed/MainActivity.kt
+++ app/src/main/java/com/nethergrim/unsplashed/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.nethergrim.unsplashed
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import org.jetbrains.anko.frameLayout
+import org.jetbrains.anko.textView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        frameLayout {
+
+            val text = textView {
+                text = "Hello"
+            }
+            text.gravity = Gravity.CENTER
+        }
     }
 }
