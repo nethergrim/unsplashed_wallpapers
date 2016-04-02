cd783deb3258a9c7f2d9ca867d12b35df3922ed7
Andrew Drobyazko <c2q9450@gmail.com>
2016-04-02 11:40:53 +0300

    Added icon to image placeholder

--- app/src/main/java/com/nethergrim/unsplashed/ui/adapters/viewHolders/MainVh.kt
+++ app/src/main/java/com/nethergrim/unsplashed/ui/adapters/viewHolders/MainVh.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder
 import com.facebook.drawee.view.SimpleDraweeView
+import com.nethergrim.unsplashed.R
 
+@Suppress("DEPRECATION")
 /**
  * @author Andrey Drobyazko (c2q9450@gmail.com).
  * All rights reserved.
@@ -21,8 +24,13 @@ class MainVh(val view: View) : RecyclerView.ViewHolder(view) {
     companion object {
         fun generate(context: Context): MainVh {
             var draweeView = SimpleDraweeView(context)
-            draweeView.layoutParams = ViewGroup.LayoutParams(-1,-2)
+            draweeView.layoutParams = ViewGroup.LayoutParams(-1, -2)
             draweeView.aspectRatio = 1.5f
+
+            val builder = GenericDraweeHierarchyBuilder(context.resources)
+            builder.fadeDuration = 200
+            builder.placeholderImage = context.resources.getDrawable(R.drawable.ic_image_black_24px)
+            draweeView.hierarchy = builder.build()
             return MainVh(draweeView)
         }
     }
