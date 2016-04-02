a0d03fe9ea7035b2d1161176c1a758e4aa7c0fcc
Andrew Drobyazko <c2q9450@gmail.com>
2016-04-02 18:43:14 +0300

    Added crashlytics

--- app/src/main/java/com/nethergrim/unsplashed/App.kt
+++ app/src/main/java/com/nethergrim/unsplashed/App.kt
@@ -1,9 +1,12 @@
 package com.nethergrim.unsplashed
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.firebase.client.Config
 import com.firebase.client.Firebase
+import io.fabric.sdk.android.Fabric
 
 /**
  * @author Andrey Drobyazko (c2q9450@gmail.com).
@@ -18,6 +21,7 @@ open class App: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        Fabric.with(this, Crashlytics(), Answers())
         Firebase.setAndroidContext(this)
         val config = Config()
         config.isPersistenceEnabled = true
